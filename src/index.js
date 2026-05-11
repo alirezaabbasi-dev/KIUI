dee7a756cadff13882a8ed0c15f1461ecd910852
Kiyan Abbasi <alirezaabbasi001@outlook.com>
2026-05-11 23:20:00 +0330

    refactor: :recycle: refactor scripts from html to external js file

--- src/index.js
+++ src/index.js
@@ -7,18 +7,34 @@ import "./effects/typewriter.js";
 window.initHeader = initHeader;
 window.initFooter = initFooter;
 
+window.addEventListener("load", () => {
+  // ? InitProject
+  (async () => {
+    const navMenusDataRes = await fetch("/allData/header.json");
+    const navMenusData = await navMenusDataRes.json();
+    initHeader(navMenusData);
 
+    const footerDataRes = await fetch("/allData/footer.json");
+    const footerData = await footerDataRes.json();
 
-//  Use KIUI Type Writer
+    const socialsDataRes = await fetch("/allData/socials.json");
+    const socialsData = await socialsDataRes.json();
 
-initKIUITypeWriter(
-  [
-    "Welcome to KIUI",
-    "A lightweight",
-    "and",
-    "Modern Web Component UI Kit.",
-    "Let’s build something amazing together . . . ",
-  ],
-  100, // typing/deleting speed
-  1000, // pause between type/delete
-);
+    const footer = { ...footerData.footer, socials: socialsData.socials };
+    initFooter(footer);
+  })();
+
+  //  Use KIUI Type Writer
+
+  initKIUITypeWriter(
+    [
+      "Welcome to KIUI",
+      "A lightweight",
+      "and",
+      "Modern Web Component UI Kit.",
+      "Let’s build something amazing together . . . ",
+    ],
+    100, // typing/deleting speed
+    1000, // pause between type/delete
+  );
+});
